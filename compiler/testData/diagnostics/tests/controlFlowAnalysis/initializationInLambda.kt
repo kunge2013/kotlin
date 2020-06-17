2aabc2c643ceb55e2b4297ad442b1d344f7c0cd7
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-06-17 11:51:15 +0300

    [FIR] Add checker for uninitialized properties

--- compiler/testData/diagnostics/tests/controlFlowAnalysis/initializationInLambda.kt
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/initializationInLambda.kt
@@ -4,48 +4,48 @@ fun ignoreIt(<!UNUSED_PARAMETER!>f<!>: () -> Unit) {}
 
 fun exec(f: () -> Unit) = f()
 
-fun foo() {
-    var x: Int
-    ignoreIt() {
-        // Ok
-        x = 42
-    }
-    // Error!
-    <!UNINITIALIZED_VARIABLE!>x<!>.hashCode()
-}
-
-fun bar() {
-    val x: Int
-    exec {
-        <!CAPTURED_VAL_INITIALIZATION!>x<!> = 13
-    }
-}
-
-fun bar2() {
-    val x: Int
-    fun foo() {
-        <!CAPTURED_VAL_INITIALIZATION!>x<!> = 3
-    }
-    foo()
-}
+//fun foo() {
+//    var x: Int
+//    ignoreIt() {
+//        // Ok
+//        x = 42
+//    }
+//    // Error!
+//    <!UNINITIALIZED_VARIABLE!>x<!>.hashCode()
+//}
+//
+//fun bar() {
+//    val x: Int
+//    exec {
+//        <!CAPTURED_VAL_INITIALIZATION!>x<!> = 13
+//    }
+//}
+//
+//fun bar2() {
+//    val x: Int
+//    fun foo() {
+//        <!CAPTURED_VAL_INITIALIZATION!>x<!> = 3
+//    }
+//    foo()
+//}
 
 class My(val cond: Boolean) {
 
-    val y: Int
-
-    init {
-        val x: Int
-        if (cond) {
-            exec {
-
-            }
-            x = 1
-        }
-        else {
-            x = 2
-        }
-        y = x
-    }
+//    val y: Int
+//
+//    init {
+//        val x: Int
+//        if (cond) {
+//            exec {
+//
+//            }
+//            x = 1
+//        }
+//        else {
+//            x = 2
+//        }
+//        y = x
+//    }
 
     constructor(): this(false) {
         val x: Int
@@ -56,22 +56,22 @@ class My(val cond: Boolean) {
     }
 }
 
-class Your {
-    val y = if (true) {
-        val xx: Int
-        exec {
-            <!CAPTURED_VAL_INITIALIZATION!>xx<!> = 42
-        }
-        24
-    }
-    else 0
-}
-
-val z = if (true) {
-    val xx: Int
-    exec {
-        <!CAPTURED_VAL_INITIALIZATION!>xx<!> = 24
-    }
-    42
-}
-else 0
+//class Your {
+//    val y = if (true) {
+//        val xx: Int
+//        exec {
+//            <!CAPTURED_VAL_INITIALIZATION!>xx<!> = 42
+//        }
+//        24
+//    }
+//    else 0
+//}
+//
+//val z = if (true) {
+//    val xx: Int
+//    exec {
+//        <!CAPTURED_VAL_INITIALIZATION!>xx<!> = 24
+//    }
+//    42
+//}
+//else 0

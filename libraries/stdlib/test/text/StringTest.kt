7f9fecd2de818819c0664e191c74dc9e6adf06d2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-06-18 16:23:15 +0300

    fixup! 'capitalize' should convert digraphs to title case #KT-38817

--- libraries/stdlib/test/text/StringTest.kt
+++ libraries/stdlib/test/text/StringTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -292,6 +292,8 @@ class StringTest {
         assertEquals("A", "a".capitalize())
         assertEquals("Abcd", "abcd".capitalize())
         assertEquals("Abcd", "Abcd".capitalize())
+        // Case mapping that results in multiple characters (validating Character.toUpperCase was not used).
+        assertEquals("SSßß", "ßßß".capitalize())
     }
 
     @Test fun decapitalize() {

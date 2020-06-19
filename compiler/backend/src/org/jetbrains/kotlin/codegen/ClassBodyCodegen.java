26e7197092f494590c76b50ae057035d55bada25
Mikhail Bogdanov <mikhael.bogdanov@jetbrains.com>
2020-06-19 15:54:23 +0200

    Fix compilation on mixed hierarchies in compatibility mode

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ClassBodyCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ClassBodyCodegen.java
@@ -239,14 +239,8 @@ public abstract class ClassBodyCodegen extends MemberCodegen<KtPureClassOrObject
             boolean isErasedInlineClass
     ) {
         // Skip Java 8 default methods
-        if (CodegenUtilKt.isDefinitelyNotDefaultImplsMethod(interfaceFun)) {
-            return;
-        }
-
-        CallableMemberDescriptor actualImplementation =
-                interfaceFun.getKind().isReal() ? interfaceFun : findImplementationFromInterface(interfaceFun);
-        assert actualImplementation != null : "Can't find actual implementation for " + interfaceFun;
-        if (JvmAnnotationUtilKt.isCallableMemberCompiledToJvmDefault(actualImplementation, state.getJvmDefaultMode())) {
+        if (CodegenUtilKt.isDefinitelyNotDefaultImplsMethod(interfaceFun) ||
+            JvmAnnotationUtilKt.checkIsCallableMemberCompiledToJvmDefault(interfaceFun, state.getJvmDefaultMode())) {
             return;
         }
 
